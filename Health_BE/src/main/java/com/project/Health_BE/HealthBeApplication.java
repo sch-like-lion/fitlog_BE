e258ea3ab0a1451ac411a468722214eb980b2c17
JHkim0609 <no1tdc2@gmail.com>
2025-07-21 20:09:27 +0900

    work

---
+++ Health_BE/src/main/java/com/project/Health_BE/HealthBeApplication.java
@@ -0,0 +1,11 @@
+package com.project.Health_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HealthBeApplication.class, args);
+	}
+}
