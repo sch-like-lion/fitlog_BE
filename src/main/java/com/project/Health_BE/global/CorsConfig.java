50c4ac9295aac32e8678158c8c7fce731caebfac
JHkim0609 <no1tdc2@gmail.com>
2025-09-03 10:32:13 +0900

    api 주소 변경

--- src/main/java/com/project/Health_BE/global/CorsConfig.java
+++ src/main/java/com/project/Health_BE/global/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 /// 현재 어떤 주소 사용중인지 모름 임시로 넣어둔것
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedOrigins("https://fitlog.iubns.net")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
