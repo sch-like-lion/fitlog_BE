b27dd4658e3902e758fede4ae91b13f13a6ab02a
conconcc <lesj9217@gmail.com>
2025-11-17 20:09:34 +0900

    feat: 티어 내 랭킹 조회 추가

--- src/main/java/com/project/Health_BE/Service/RankService.java
+++ src/main/java/com/project/Health_BE/Service/RankService.java
@@ -8,6 +8,7 @@ import com.project.Health_BE.Repository.UserRepository;
 import com.project.Health_BE.Security.JwtTokenProvider;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,10 +19,11 @@ public class RankService {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
-    public UserRankInfoResponseDto getUserRankInfo(String jwtToken) {
-        Long userId = jwtTokenProvider.getUserId(jwtToken);
+    public UserRankInfoResponseDto getUserRankInfo(Authentication jwtToken) {
+        String userIdInToken=jwtToken.getName();
+        Long userId= Long.parseLong(userIdInToken);
         UserEntity user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다. userId=" + userId));
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다. userId=" + userIdInToken));
 
         int userScore = user.getTotalPoint();
 
@@ -47,14 +49,37 @@ public class RankService {
                 .overallRank(overallRank)
                 .build();
     }
-
+    // 전체 랭킹 조회
     @Transactional
     public UserRankInfoResponseDto getOverallRank(String jwtToken) {
-        Long userId = jwtTokenProvider.getUserId(jwtToken);
+        String userIdInToken = jwtTokenProvider.getUserId(jwtToken);
+        Long userId = Long.parseLong(userIdInToken);
         Long overallRank =  userRepository.findOverallRankByUserId(userId);
         return UserRankInfoResponseDto.builder()
                 .overallRank(overallRank)
                 .build();
+    }
+    // 티어 내 랭킹 조회
+    public UserRankInfoResponseDto getRankInTier(String jwtToken) {
+        String userIdInToken = jwtTokenProvider.getUserId(jwtToken);
+        Long userId = Long.parseLong(userIdInToken);
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다. userId=" + userIdInToken));
 
+        int userScore = user.getTotalPoint();
+
+        RankEntity currentTier = rankRepository.findTierByScore(userScore)
+                .orElseThrow(() -> new IllegalArgumentException("해당 점수의 티어 정보를 찾을 수 없습니다."));
+
+        int minPoint = currentTier.getMinScore();
+        int maxPoint = rankRepository.findNextTier(minPoint)
+                .map(nextTier -> nextTier.getMinScore() - 1)
+                .orElse(Integer.MAX_VALUE);
+
+        Long rankInTier = userRepository.findRankInTier(userId, minPoint, maxPoint);
+
+        return UserRankInfoResponseDto.builder()
+                .rankInTier(rankInTier)
+                .build();
     }
 }
