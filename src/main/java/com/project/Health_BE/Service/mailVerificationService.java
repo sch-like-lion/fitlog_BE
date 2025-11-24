266493ae128c2d4eecce3558b81266f33e6100bd
jhkim <no1tdc2@gmail.com>
2025-11-24 17:38:02 +0900

    email 인증 없이 회원가입, 비밀번호 찾기 오류 수정

--- src/main/java/com/project/Health_BE/Service/mailVerificationService.java
+++ src/main/java/com/project/Health_BE/Service/mailVerificationService.java
@@ -1,22 +1,28 @@
 package com.project.Health_BE.Service;
 
 import com.project.Health_BE.Dto.mailVerificationDto;
+import com.project.Health_BE.Entity.Emailverification;
+import com.project.Health_BE.Exception.UserNotFoundException;
+import com.project.Health_BE.Repository.EmailverificatonRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class mailVerificationService {
     @Autowired
     private JavaMailSender mailSender;
-    private final mailVerificationDto mailDto = new mailVerificationDto();
+    private final EmailverificatonRepository emailRepository;
 
-    public String Sendmail(String email) { ///사용자 email
+    public String Sendmail(String email) {///사용자 email
+        mailVerificationDto mailDto = new mailVerificationDto();
         SimpleMailMessage message = new SimpleMailMessage();
         Random code = new Random();
         mailDto.setEmail(email);
@@ -27,15 +33,30 @@ public class mailVerificationService {
         message.setSubject("이메일 인증번호입니다");
         message.setText("인증번호는: " + mailDto.getVerificationCode() + "입니다.");
         mailSender.send(message);
+
+        Emailverification entity = emailRepository.findByEmail(email)
+                .orElseGet(() -> Emailverification.builder()
+                        .email(email)
+                        .code(mailDto.getVerificationCode())
+                        .updatetime(LocalDateTime.now())
+                        .verify(false)
+                        .build()
+                );
+        emailRepository.save(entity);
         return "전송 완료" + email;
     }
 
-    public mailVerificationDto Verification(String code) { ///사용자가 입력한 코드 받기
-        if(mailDto.getVerificationCode().equals(code)) {
-            mailDto.setMailcheck(true);
-            return mailDto;
+    public mailVerificationDto Verification(mailVerificationDto dto) {
+
+        Emailverification entity = emailRepository.findByEmail(dto.getEmail()).orElseThrow(() -> new UserNotFoundException("email을 찾을 수 없습니다."));
+        if (ChronoUnit.MINUTES.between(entity.getUpdatetime(), LocalDateTime.now()) > 5) throw new RuntimeException("인증코드가 만료되었습니다."); ///사용자가 입력한 코드 받기
+        if(dto.getVerificationCode().equals(dto.getVerificationCode())) {
+            entity.builder().verify(true).updatetime(LocalDateTime.now()).code(null);
+            emailRepository.save(entity);
+            dto.setMailcheck(true);
+            return dto;
         }
-        return mailDto;
+        return dto;
     }
 
 }
